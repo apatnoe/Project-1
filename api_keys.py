dd8f852a23763541b07436df33d97c6a340e7a8e
IIVIIIII <75957420+IIVIIIII@users.noreply.github.com>
2021-02-20 10:24:05 -0800

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-yelp_key = "b_0GhfBLfM5Iec2wRJjcQ7EpBnoXyQl_Q5z9Q_vWCqlTBeh0wjEODROAZAFakmeASmzC-WoGWE6Bk3T5viQIXLzErVsgu02FMy0AkikMnEwWhAteCqZDaxBBJcMhYHYx"
-
-# Google API Key
-g_key = "AIzaSyCv1BWR1Jm0cxoY8oqWxYHWU2g6Aq91SvE"
